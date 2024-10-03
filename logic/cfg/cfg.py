156fe7dfb4ba1fae06e5058dddafdecff82b1f32
Ailurusa <eln.surkova@gmail.com>
2024-10-03 21:44:07 +0200

    Add config parser

---
+++ logic/cfg/cfg.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+from yaml import load, Loader
+
+CFG_LOCATION = 'resources/config.yaml'
+
+
+class Config:
+    """Singleton configuration keeper"""
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Config, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self):
+        self._raw_data = self.__read_cfg_file()
+
+    def __read_cfg_file(self):
+        if hasattr(self, '_raw_data'):
+            return self._raw_data
+        return load(self.cfg_file.open(), Loader=Loader)
+
+    @property
+    def cfg_file(self):
+        location = Path(__file__).parent.parent / CFG_LOCATION
+        assert location.exists(), f'Config file not found. Expected location: {location}'
+        return location
+
+    @property
+    def host(self):
+        return f'http://{self.server_ip}:{self.port}'
+
+    @property
+    def password(self):
+        return self.__get_cfg_attr('password')
+
+    @property
+    def port(self):
+        return self.__get_cfg_attr('port')
+
+    @property
+    def server_ip(self):
+        return self.__get_cfg_attr('server_ip')
+
+    @property
+    def username(self):
+        return self.__get_cfg_attr('username')
+
+    def __get_cfg_attr(self, attr_name: str):
+        if attr_name not in self._raw_data:
+            raise AttributeError(f'Next attribute not found in config file: {attr_name}')
+        return self._raw_data[attr_name]
